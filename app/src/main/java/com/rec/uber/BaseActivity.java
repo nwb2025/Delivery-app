e1d5e7eb807608ab4d30904bc5fa68e6a94848da
New Wealth Builder <newwealthbuilder@MacBook-Pro-M1.lan>
2022-12-13 09:00:59 -0800

    first commit

---
+++ app/src/main/java/com/rec/uber/BaseActivity.java
@@ -0,0 +1,32 @@
+package com.rec.uber;
+
+import android.app.Dialog;
+import android.util.Log;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+    private Dialog progressDialog;
+
+    private void showProgressDialog(String text) {
+        progressDialog = new Dialog(this);
+        progressDialog.setContentView(R.layout.progress_dialog);
+        TextView textView = progressDialog.findViewById(R.id.tvProgressText);
+        textView.setText(text);
+        progressDialog.setCancelable(false);
+        progressDialog.setCanceledOnTouchOutside(false);
+        progressDialog.show();
+    }
+
+    private void hideProgressDialog() {
+        if(progressDialog != null) {
+            progressDialog.dismiss();
+        }
+        else {
+            Log.d("BaseActivity ", "Progess dialog wasn't initialized ");
+        }
+    }
+
+
+}
+
